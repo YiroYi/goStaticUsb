b0b0b39cc5adb4a0885d424456b9e99dc06f953e
YiroYi <yiroyi@live.com>
2023-09-27 15:03:04 -0600

    Attempt to static link

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 // #cgo CFLAGS:-I/opt/homebrew/Cellar/libusb/1.0.26/include/libusb-1.0
-// #cgo LDFLAGS:-L/opt/homebrew/Cellar/libusb/1.0.26/lib -lusb-1.0
+// #cgo LDFLAGS: -L. -lusb-1.0
 // #include "/opt/homebrew/Cellar/libusb/1.0.26/include/libusb-1.0/libusb.h"
 import "C"
 
