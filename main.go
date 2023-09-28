5636dfc93132189585565ef1e6ec78408264f813
YiroYi <yiroyi@live.com>
2023-09-28 14:11:33 -0600

    Add logic to test Bixolon

--- main.go
+++ main.go
@@ -7,9 +7,56 @@ import "C"
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/google/gousb"
 )
 
 func main() {
-	ctx := C.libusb_init(nil)
-	fmt.Println("Hello")
+
+	ctx := gousb.NewContext()
+	// A Context manages all resources necessary for communicating with USB devices.
+	//Through the Context users can iterate over available USB devices.
+	defer ctx.Close()
+	// The "defer" keyword will allow to call Close() when the main() is closed with this
+	// we will avoid memory leaks if the connection is closed incorrectly or interrupted
+
+	// Find the Zebra printer by its USB Vendor ID and Product ID
+	dev, err := ctx.OpenDeviceWithVIDPID(gousb.ID(0x1504), gousb.ID(0x0037))
+	fmt.Println(dev.Desc)
+	if err != nil {
+		log.Fatalf("Could not open device: %v", err)
+	}
+	defer dev.Close() // this method what will be do is to close the connection when the main function is ready
+
+	// Set up the default configuration
+	cfg, err := dev.Config(1)
+	if err != nil {
+		log.Fatalf("Could not get configuration: %v", err)
+	}
+	defer cfg.Close()
+
+	// Claim an interface for communication
+	intf, err := cfg.Interface(0, 0)
+	if err != nil {
+		log.Fatalf("Could not claim interface: %v", err)
+	}
+	defer intf.Close()
+
+	// Send ZPL commands to the printer
+	// zplCommand := "~Hi"
+	zplCommand := "^XA^FO100,50^ADN,36,20^FDHello, Zebra Printer!^FS^XZ"
+	outEndpoint, err := intf.OutEndpoint(2)
+
+	if err != nil {
+		log.Fatalf("Failed to send ZPL command: %v", err)
+	}
+
+	_, err = outEndpoint.Write([]byte(zplCommand))
+
+	if err != nil {
+		fmt.Println("Fatal Error")
+	}
+
+	fmt.Println("ZPL command sent successfully.")
 }
